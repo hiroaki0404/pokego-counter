d5ffeeb85d5b1fec2fe379a3ff38d3dee6495965
Hiroaki Abe <hiroaki0404@gmailcom>
2020-05-31 21:16:07 +0900

    Use LINE SDK.

--- go-counter.rb
+++ go-counter.rb
@@ -1,6 +1,55 @@
 require 'sinatra'
+require 'line/bot'
 
+def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+end
+
+def decode_image(path)
+    return "not implimented"
+end
 
 get '/' do
     return "OK"
+end
+
+post '/count' do
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+        error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+          tf.close
+          msg = decode_image(tf.path)
+          message = {
+              type: 'text',
+              text: msg
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+  
+    "OK"  
 end
